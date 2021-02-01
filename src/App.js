f6f058fe08fe38c7b498a9c7bcf79bfe412d5910
Aynun <nissasetiawan4@gmail.com>
2021-02-01 11:04:10 +0700

    Add App component (navbar,main,footer) and style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import './assets.css';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section class="navbar">
+        <div class="navbar">
+          <h1 class="navbar">RecipeBooks</h1>
+        </div>
+      </section>
+
+      <section class="main">
+      </section>
+
+      <footer class="footer">
+        <p>Developed by Aynun Nissa</p>
+      </footer>
+    </React.Fragment>
   );
 }
 
